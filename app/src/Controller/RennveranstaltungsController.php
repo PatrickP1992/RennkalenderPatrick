fad11d3e55cc408b96fd51894d7bf9a1786939a5
PatrickP1992 <42120798+PatrickP1992@users.noreply.github.com>
2024-07-20 09:40:41 +0200

    Rennveranstaltung mit Controller erstellen und anzeigen
    Habe hier ausprobiert, ob ich eine Rennveranstaltung erstellen und anzeigen kann

--- app/src/Controller/RennveranstaltungsController.php
+++ app/src/Controller/RennveranstaltungsController.php
@@ -2,17 +2,46 @@
 
 namespace App\Controller;
 
+use App\Entity\Rennveranstaltung;
+use App\Repository\RennveranstaltungRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\Persistence\ManagerRegistry;
 
+#[Route('/rennveranstaltungen', name: 'rennveranstaltungen.')]
 class RennveranstaltungsController extends AbstractController
 {
-    #[Route('/rennveranstaltungs', name: 'app_rennveranstaltungs')]
-    public function index(): Response
+    private $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine)
     {
+        $this->doctrine = $doctrine;
+    }
+
+    #[Route('/', name: 'bearbeiten')]
+    public function index(RennveranstaltungRepository $rennveranstaltungRepository): Response
+    {
+        $rennveranstaltungen = $rennveranstaltungRepository->findAll();
+
         return $this->render('rennveranstaltungs/index.html.twig', [
-            'controller_name' => 'RennveranstaltungsController',
+            'rennveranstaltungen' => $rennveranstaltungen,
         ]);
+    }
+
+    #[Route('/anlegen', name: 'anlegen')]
+    public function anlegen(Request $request)
+    {
+        $rennveranstaltung = new Rennveranstaltung();
+        $rennveranstaltung->setName('Fomel 1');
+
+        // Entity Manager
+        $em = $this->doctrine->getManager();
+        $em->persist($rennveranstaltung);
+        $em->flush();
+
+        // Response
+        return new Response('Rennveranstaltung wurde angelegt');
     }
 }
