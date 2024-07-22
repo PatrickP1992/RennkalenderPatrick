e1e6af06d1895f2fb9c60965e82eb831cee24c13
PatrickP1992 <42120798+PatrickP1992@users.noreply.github.com>
2024-07-22 15:13:03 +0200

    AssetMapper installiert
    Zum Verwalten von Assets

--- app/src/Controller/MainController.php
+++ app/src/Controller/MainController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Repository\RennveranstaltungRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/', name: 'app_main')]
-    public function index(): Response
+    private $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine)
     {
+        $this->doctrine = $doctrine;
+    }
+    #[Route('/', name: 'app_main')]
+    public function index(RennveranstaltungRepository $rennveranstaltungRepository): Response
+    {
+        $rennveranstaltungen = $rennveranstaltungRepository->findAll();
+
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'rennveranstaltungen' => $rennveranstaltungen,
         ]);
     }
 }
